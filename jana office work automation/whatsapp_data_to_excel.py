2c6ad80342d328772a758c49f3567d7a310638c7
vinayak-vc85 <vinaone.thr@gmail.com>
2023-05-12 11:03:25 +0530

    intial committed

--- jana office work automation/whatsapp_data_to_excel.py
+++ jana office work automation/whatsapp_data_to_excel.py
@@ -1,2 +1,22 @@
 import time
-from selenium import webdriver
+import pandas as pd
+
+
+
+myvars = {}
+data_lst=[]
+with open("input.txt") as myfile:
+    for line in myfile:
+        name, var = line.partition(":")[::2]
+        myvars[name.strip()] = var.strip()
+        
+        if name.strip() =='':
+            print('Inside space')
+            print(myvars)
+print(data_lst)
+
+# print(myvars['SITE NAME'])
+# for val in data_lst:
+#     print(val)
+#     print("#########################")
+#     # print(pd.DataFrame.from_dict(val) )
